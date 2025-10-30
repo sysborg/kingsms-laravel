9192a2d336a3907deedaed942110b16b13a21845
Anderson Arruda <andmarruda@gmail.com>
2025-10-30 17:10:31 +0000

    Hotfix: Added anonymousnotifiable

--- Channels/KingsmsChannel.php
+++ Channels/KingsmsChannel.php
@@ -3,13 +3,19 @@ namespace Sysborg\KingSMS\Channels;
 
 use Illuminate\Notifications\Notification;
 use Facades\Sysborg\KingSMS\Services\KingSMS;
+use Illuminate\Notifications\AnonymousNotifiable;
 
 class KingsmsChannel
 {
     public function send($notifiable, Notification $notification)
     {
         $message = $notification->toKingsms($notifiable);
-        $phone = $notifiable->routeNotificationForKingsms();
+        if ($notifiable instanceof AnonymousNotifiable) {
+            $phone = $notifiable->routes['kingsms'];
+        } else {
+            $phone = $notifiable->routeNotificationForKingsms();
+        }
+
         KingSMS::sendSms($phone, $message);
     }
 }
